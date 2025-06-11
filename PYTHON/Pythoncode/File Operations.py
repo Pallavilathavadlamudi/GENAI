f3d5cdce8a10b14b7215c67c8f51553476e11e20
Pallavilathavadlamudi <vadlamudipallavilatha@gmail.com>
2025-06-11 15:27:41 -0400

    File Operations

--- PYTHON/Pythoncode/File Operations.py
+++ PYTHON/Pythoncode/File Operations.py
@@ -6,28 +6,42 @@ with open("File Operations.txt","w") as file:
     file.write("This is a test file. \n")
     file.write("This file is used to demonstrate file operations in python. \n")
 
-    #opening the file in read mode
+#opening the file in read mode
+
 with open ("File Operations.txt","r") as file:
     content = file.read()
     print("Output:1")
     print(content)
 
-    #Opening the file in append mode
+#Opening the file in append mode
+
 with open("File Operations.txt","a") as file:
         file.write("This is an appended line. \n")
     
-    #Reading the file again to see the appended content
+#Reading the file again to see the appended content
+
 with open("File Operations.txt","r") as file:
         content = file.read()
         print("Output:2")
         print(content)
+
+#Opening the file in Reading and writing mode
+with open("File Operations.txt","r+") as file:
+    content = file.read()
+    print("output:3")
+    print(content)
+    file.write("this is a line added ib r+ mode. \n")
+    #Reading the file again to see the content after writing in r+ mode
+    file.seek(0) # Move the cursor to the beginning of the file
+    content = file.read()
+    print(content)
     
-    # Writing to a binary file
+# Writing to a binary file
 with open("Binary File Operations.txt","wb") as file:
     file.write(b"This is a binary file. \n")
     file.write(b"This file is used to demonstrate binary file operations in python. \n")
 
-    #Reading the binary file again to see the content
+#Reading the binary file again to see the content
 with open("Binary File Operations.txt", "rb") as file:
     binary_content = file.read()
     print("Output:4")
